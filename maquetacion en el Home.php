e2cff362981553a72b6d583ef57feb60bc058094
Adiel <adielp89@gmail.com>
2018-01-09 12:47:06 -0500

    Home.php
    Este es el Home

---
+++ maquetacion en el Home.php
@@ -0,0 +1,18 @@
+<div id="newsmag-homepage-slider">
+			<div class="row slid">
+				<div class="col-sm-6 slider-izquierdo">
+					<?php
+					get_template_part( 'template-parts/cosita' );
+					//dynamic_sidebar( 'homepage-slider' );
+					?>
+				</div>
+				<div class="col-sm-6 slider-derecho">
+					<?php
+					get_template_part( 'template-parts/cosit' );
+					//dynamic_sidebar( 'homepage-slider' );
+					?>
+				</div>
+				<div class="row slider-information">
+				</div>
+       </div>
+		</div>
